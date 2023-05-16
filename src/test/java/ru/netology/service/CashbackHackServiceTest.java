82781ece9655b76726407d38974cb8e3a72ea27d
MaryDi <28907183@mail.ru>
2023-05-16 20:28:41 +0200

    add two branch

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,46 @@
+package ru.netology.service;
+
+public class CashbackHackServiceTest {
+    @org.junit.Test
+    public void WhenBuying500Rubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(expected,actual);
+
+    }
+    @org.junit.Test
+    public void WhenBuying1000Rubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(expected,actual);
+    }
+    @org.junit.Test
+    public void WhenBuying1500Rubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(expected,actual);
+    }
+
+    @org.junit.Test
+    public void WhenBuying999Rubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(expected,actual);
+    }
+    @org.junit.Test
+    public void WhenBuying1001Rubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int expected = 999;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(expected,actual);
+    }
+}
